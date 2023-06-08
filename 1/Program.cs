e19298341ab1e82fe6537ad101aa67f8db8848cc
Alan <alan199.ru@mail.ru>
2023-06-08 20:28:45 +0300

    1

--- 1/Program.cs
+++ 1/Program.cs
@@ -4,7 +4,7 @@
 9012 -> 12*/
 Console.Clear();
 Console.WriteLine("Enter number:");
-int num = int.Parse(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"сумму цифр в числе ->{num} равно {GetSum(num)}");
 
 int GetSum(int n){
